2738571b2b66fd668174823cd2168d7bfc0cac67
Your Name <a0908647929@gmail.com>
2025-07-26 12:19:54 +0800

    frontend init.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./global/reset.css";
+import Home from "./pages/home/home.jsx";
+import RouterManager from "./global/routerManager.jsx";
+
+import Test from "./pages/test.jsx";
+
+function App() {
+  return (
+    <>
+      <RouterManager />
+    </>
+  );
+}
+
+export default App;
